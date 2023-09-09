64b5a63f1ada55d011a425af955a9beb33068745
narottamchy <narottamchy5134@gmail.com>
2023-09-10 00:23:08 +0530

    img change

--- components/Projects/projectData.tsx
+++ components/Projects/projectData.tsx
@@ -6,7 +6,7 @@ const projectData: Project[] = [
     title: "NewsHub",
     description: {desc:"Stay Informed with DailyPureNews: A ReactJS News Website Delivering Fresh Updates via API Fetch",lang:"React JS, Bootstrap, API, CSS, HTML, JavaScript"},
     github:"https://github.com/Narottamchy/NewsHub",
-    image: "/images/skills/newshub.png",
+    image: "/images/skills/Newshub.png",
     live: "https://nnewshub.netlify.app/",
   },
   {
@@ -14,7 +14,7 @@ const projectData: Project[] = [
     title: "Leets Chat",
     description: {desc:"Group Chat Web App: Seamless Conversations with Google Account Authentication",lang:"Google Authentication, React JS, Bootstrap, Firebase"},
     github: "https://github.com/Narottamchy/Leetschat",
-    image: "/images/skills/leetschat.png",
+    image: "/images/skills/LeetsChat.png",
     live: "https://leetschat.netlify.app/",
   },
   {
@@ -22,7 +22,7 @@ const projectData: Project[] = [
     title: "Naturaleza",
     description: {desc:"Naturaleza: Your Organic Tech Guide for Informed Product Choices and Education",lang:"React JS, Next Js, Api, CSS, HTML, JavaScript"},
     github: "https://github.com/Narottamchy/Naturaleza/tree/main/source/naturaleza",
-    image: "/images/skills/naturaleza.png",
+    image: "/images/skills/Naturaleza.png",
     live: "https://naturaleza-self.vercel.app/",
   },
   {
@@ -30,7 +30,7 @@ const projectData: Project[] = [
     title: "TeleChats",
     description: {desc:"Introducing TeleChat: Your Personal, Secure, Real-time Connection Solution. Revolutionizing Communication with Privacy and User Experience in Mind.",lang:"Socket.io, React JS, Next JS, Node JS, Express JS, MongoDB, Tailwind CSS"},
     github: "https://github.com/Narottamchy/telechat",
-    image: "/images/skills/telechat.png",
+    image: "/images/skills/Telechat.png",
     live: "https://telechats.netlify.app/",
   },
 ];
